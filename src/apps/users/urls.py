76aa6a88f24fdf7744b45bbf04cc7d53389463ed
tima <temurbektojiyev1602@gmail.com>
2026-03-07 21:12:02 +0500

    templates qoshildi

---
+++ src/apps/users/urls.py
@@ -0,0 +1,9 @@
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.dashboard_page, name="dashboard_page"),
+    path("auth/login/", views.login_page, name="login_page"),
+    path("logout/", views.login_page, name="login_page"),
+]
