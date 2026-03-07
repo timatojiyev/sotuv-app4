ac021a9d33e12d49b48fee8be29299206d22cdda
ziyodullo <ziyodullodev@gmail.com>
2026-03-07 19:56:33 +0500

    add: all models to register admin panel

--- src/apps/order/admin.py
+++ src/apps/order/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Order, OrderItem, Cart
 
 # Register your models here.
+
+admin.site.register(Order)
+admin.site.register(Cart)
+admin.site.register(OrderItem)
