96f6fc8754ff94c53055aba0a48ee4885bfbc8bc
tima <temurbektojiyev1602@gmail.com>
2026-03-10 20:21:27 +0500

    ddd

--- src/apps/product/views.py
+++ src/apps/product/views.py
@@ -2,5 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def monitoring_page(request):
     return render(request, "monitoring.html")
+
+
+def products_page(request):
+    return render(request, "products.html")
+
+
+def products_create_page(request):
+    return render(request, "products-create.html")
